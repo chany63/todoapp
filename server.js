54c39f491e4545fc1e3b6565a0d424ec25250b10
chany63 <chany63@korea.ac.kr>
2023-01-31 00:56:25 +0900

    initialize server pages

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express'); //설치한 라이브러리 첨부
 const app = express(); //라이브러리 이용해서 객체 생성
 
-app.listen(8080, function(){
+app.listen(8080, function () {
     console.log('listening on 8080')
+});
+
+app.get('/write', function (req, res) {
+    res.sendFile(__dirname+ '/write.html');
+});
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html')
 });
